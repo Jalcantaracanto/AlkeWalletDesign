4360cf5e3ebf1ccd452fd6118d98fd9c7f8dab8b
Jalcantaracanto <javier.alcantara.canto@gmail.com>
2024-05-20 20:12:09 -0400

    dataset

--- app/src/main/java/com/example/alkewallet/feature/data/local/TransactionDataSet.kt
+++ app/src/main/java/com/example/alkewallet/feature/data/local/TransactionDataSet.kt
@@ -1,4 +1,30 @@
 package com.example.alkewallet.feature.data.local
 
+import com.example.alkewallet.feature.data.model.Transaction
+import com.example.alkewallet.feature.data.model.Transferencia
+
 class TransactionDataSet {
+
+    private val transactions: MutableList<Transaction>
+
+    init {
+        transactions = createUserDataSet()
+    }
+
+    private fun createUserDataSet(): MutableList<Transaction> {
+        return mutableListOf(
+            Transaction(1, 500.00, "Oct 14, 10:24 AM", 100, 101),
+            Transaction(2, 1500.00, "Oct 16, 11:00 AM", 100, 102),
+            Transaction(3, 2500.00, "Oct 17, 12:00 PM", 101, 100),
+
+            )
+    }
+    fun getAllTransaction() : MutableList<Transaction> {
+        return transactions
+    }
+
+    fun createNewTransaction(transaction : Transaction) {
+        transactions.add(transaction)
+    }
+
 }
